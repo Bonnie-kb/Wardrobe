7ba15bd92168ae66ccb0a1dc3a35fdf8658a69e2
Bonnie-kb <103755588+Bonnie-kb@users.noreply.github.com>
2025-03-27 22:44:07 +0530

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import WardrobePlanner from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WardrobePlanner />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
